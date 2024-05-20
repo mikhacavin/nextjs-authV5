8913174176efc582c21b596458634a92669085d8
mikhacavin <mikhacavinarthur@gmail.com>
2024-05-20 11:42:46 +0700

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
-import Appbar from "./Appbar";
+import Appbar from "./appBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
